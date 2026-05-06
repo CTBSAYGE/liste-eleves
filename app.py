e5ffe57f44009f909f5908d516439af2238bcd3c
CTBSAYGE <tcheutchouamalachie@gmail.com>
2026-05-06 14:42:11 +0200

    add me

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ eleves = [
     {"prenom": "Nora", "nom": "Boukal", "github": "loic-prof"},
     {"prenom": "Dhiaeddine", "nom": "Hafsaoui", "github": "dhiaeddinehafsaoui-create", "groupe": "Groupe 4"},
     {"prenom": "jeff", "nom": "Ngoy", "github": "Jeff-victor"},
+    {"prenom":"leroy", "nom":"Fonkou,"github":"CTBSAYGE"},
 ]
     
 TEMPLATE = """
